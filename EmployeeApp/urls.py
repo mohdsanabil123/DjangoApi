a1684eb3686a38235349de6aab31c40d53fea8f4
Mohd Sanabil <mohdsanabil4557@gmail.com>
2023-08-06 15:42:21 +0530

    Update url

--- EmployeeApp/urls.py
+++ EmployeeApp/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     re_path(r'^department/$', views.departmentApi),
     re_path(r"^department/([0-9]+)$", views.departmentApi),
     
-    re_path(r'^employee/$', views.departmentApi),
-    re_path(r"^employee/([0-9]+)$", views.departmentApi),
+    re_path(r'^employee/$', views.employeeApi),
+    re_path(r"^employee/([0-9]+)$", views.employeeApi),
     
     re_path(r'^Employee/SaveFile$', views.SaveFile)
 ]
