9a995352c31ebbe6f936f629e407a6979e893706
Mohd Sanabil <mohdsanabil4557@gmail.com>
2023-08-06 15:15:07 +0530

    Initial Commit

---
+++ EmployeeApp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Departments( models.Model ):
+    DepartmentId = models.AutoField(primary_key=True)
+    DepartmentName = models.CharField(max_length=100)
+    
+
+class Employees( models.Model ):
+    EmployeesId = models.AutoField(primary_key=True)
+    EmployeeName = models.CharField(max_length=100)
+    Department = models.CharField(max_length=100)
+    DateOfJoining = models.DateField()
+    PhotoFieldName = models.CharField(max_length=100)
---
+++ EmployeeApp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import re_path
+from . import views
+
+urlpatterns = [
+    re_path(r'^department/$', views.departmentApi),
+    re_path(r"^department/([0-9]+)$", views.departmentApi),
+    
+    re_path(r'^employee/$', views.departmentApi),
+    re_path(r"^employee/([0-9]+)$", views.departmentApi),
+    
+    re_path(r'^Employee/SaveFile$', views.SaveFile)
+]
